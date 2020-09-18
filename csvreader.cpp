3492c87c3d1a66d8d8a34698baee8f8739ef2461
iluxa_spb <ilya_15@mail.ru>
2020-09-18 17:24:13 +0300

    Add tests

--- csvreader.cpp
+++ csvreader.cpp
@@ -8,14 +8,20 @@ int main(int argc, char * argv[])
     if (argc <= 1) {
         std::cout << "Error! The output file is not specified.";
         return 0;
-    } else  if(argc > 2){
+    } else  if(argc > 3){
         std::cout << "Error! Too many arguments.";
-        return 0;
+        return 1;
     }
-    std::string filename(argv[1]);
-    CsvHandler* handler_ = new CsvHandler(filename);
+    std::string infilename(argv[1]);
+    std::string outfilename;
+    CsvHandler* handler_ = new CsvHandler(infilename);
     handler_->calculate_values();
     handler_->write_csv();
-    handler_->write_csv_to_file("out.csv");
+    if (argc == 2){
+        outfilename = argv[2];
+        handler_->write_csv_to_file(outfilename);
+    }
+    else
+        handler_->write_csv_to_file("out.csv");
     return 0;
 }
